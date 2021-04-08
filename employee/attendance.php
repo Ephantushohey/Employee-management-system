214296ba8b52d52f6799666abae109b85a459e62
jackiekariuki <73882987+jackiekariuki@users.noreply.github.com>
2021-04-08 14:34:00 +0300

    Update attendance.php

--- employee/attendance.php
+++ employee/attendance.php
@@ -28,25 +28,121 @@
 			</ul>
 		</nav>
 	</header>
-   <button type="button" id="inline" class="btn btn-danger mb-3" data-model-fixed="true" data-content="inline"><i class="fa fa-fw fa-file-alt"></i> CLock in</button>
-   <?php
-// PHP program to pop an alert
-// message box on the screen
-  
-// Display the alert box 
-echo '<script>alert("you have successfully clocked in")</script>';
-  
-?>
-<button type="button" id="inline" class="btn btn-danger mb-3" data-model-fixed="true" data-content="inline"><i class="fa fa-fw fa-file-alt"></i> CLock out</button>
-<?php
-// PHP program to pop an alert
-// message box on the screen
-  
-// Display the alert box 
-echo '<script>alert("you have successfully clocked out")</script>';
-  
-?>
-	
+    <!-- Main content -->
+    <section class="content">
+      <?php
+        if(isset($_SESSION['error'])){
+          echo "
+            <div class='alert alert-danger alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+              <h4><i class='icon fa fa-warning'></i> Error!</h4>
+              ".$_SESSION['error']."
+            </div>
+          ";
+          unset($_SESSION['error']);
+        }
+        if(isset($_SESSION['success'])){
+          echo "
+            <div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+              <h4><i class='icon fa fa-check'></i> Success!</h4>
+              ".$_SESSION['success']."
+            </div>
+          ";
+          unset($_SESSION['success']);
+        }
+      ?>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <a href="#addnew" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
+            </div>
+            <div class="box-body">
+              <table id="example1" class="table table-bordered">
+                <thead>
+                  <th class="hidden"></th>
+                  <th>Date</th>
+                  <th>Employee ID</th>
+                  <th>Name</th>
+                  <th>Time In</th>
+                  <th>Time Out</th>
+                  <th>Tools</th>
+                </thead>
+                <tbody>
+                  <?php
+                    $sql = "SELECT *, employees.employee_id AS empid, attendance.id AS attid FROM attendance LEFT JOIN employees ON employees.id=attendance.employee_id ORDER BY attendance.date DESC, attendance.time_in DESC";
+                    $query = $conn->query($sql);
+                    while($row = $query->fetch_assoc()){
+                      $status = ($row['status'])?'<span class="label label-warning pull-right">ontime</span>':'<span class="label label-danger pull-right">late</span>';
+                      echo "
+                        <tr>
+                          <td class='hidden'></td>
+                          <td>".date('M d, Y', strtotime($row['date']))."</td>
+                          <td>".$row['empid']."</td>
+                          <td>".$row['firstname'].' '.$row['lastname']."</td>
+                          <td>".date('h:i A', strtotime($row['time_in'])).$status."</td>
+                          <td>".date('h:i A', strtotime($row['time_out']))."</td>
+                          <td>
+                            <button class='btn btn-success btn-sm btn-flat edit' data-id='".$row['attid']."'><i class='fa fa-edit'></i> Edit</button>
+                            <button class='btn btn-danger btn-sm btn-flat delete' data-id='".$row['attid']."'><i class='fa fa-trash'></i> Delete</button>
+                          </td>
+                        </tr>
+                      ";
+                    }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>   
+  </div>
+    
+  <?php include 'includes/footer.php'; ?>
+  <?php include 'includes/attendance_modal.php'; ?>
+</div>
+<?php include 'includes/scripts.php'; ?>
+<script>
+$(function(){
+  $('.edit').click(function(e){
+    e.preventDefault();
+    $('#edit').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
+  });
+
+  $('.delete').click(function(e){
+    e.preventDefault();
+    $('#delete').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
+  });
+});
+
+function getRow(id){
+  $.ajax({
+    type: 'POST',
+    url: 'attendance_row.php',
+    data: {id:id},
+    dataType: 'json',
+    success: function(response){
+      $('#datepicker_edit').val(response.date);
+      $('#attendance_date').html(response.date);
+      $('#edit_time_in').val(response.time_in);
+      $('#edit_time_out').val(response.time_out);
+      $('#attid').val(response.attid);
+      $('#employee_name').html(response.firstname+' '+response.lastname);
+      $('#del_attid').val(response.attid);
+      $('#del_employee_name').html(response.firstname+' '+response.lastname);
+    }
+  });
+}
+</script>
+</body>
+</html>
+
    </body>
    </html>
 	
